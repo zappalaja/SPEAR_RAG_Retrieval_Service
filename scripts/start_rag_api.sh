13dba2669efa44255106231bfccbda12f93ad940
Jake Zappala <zappalajake@gmail.com>
2026-01-15 06:18:05 -0500

    Update start_rag_api.sh

--- scripts/start_rag_api.sh
+++ scripts/start_rag_api.sh
@@ -13,8 +13,8 @@ conda activate rag
 # RAG configuration
 # --------------------------------------------------
 export CHROMA_PERSIST_DIR="/home/zappalaj/Bot_Test/ingestion_rag/SPEAR-RAG-Ingestion/chroma_db"
-export CHROMA_COLLECTION="nougat_merged"
-export EMBED_MODEL="sentence-transformers/all-MiniLM-L6-v2"
+export CHROMA_COLLECTION="nougat_merged" # Likely you will not need to edit this. This is the collection inside your ChromaDB.
+export EMBED_MODEL="sentence-transformers/all-MiniLM-L6-v2" # Likely you will not need to edit this. This is the embedding model.
 
 # --------------------------------------------------
 # Start FastAPI
